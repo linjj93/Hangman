c326b820a05162a0858f1a41064ab2f2b6d712b9
Jinjia Lin <linjj93@hotmail.com>
2018-11-13 23:17:54 +0800

    commit for new fixes

--- portfolio-app.js
+++ portfolio-app.js
@@ -7,19 +7,32 @@ const schoolP = document.getElementsByClassName("school")[0];
 const workP = document.getElementsByClassName("work")[0];
 
 introButton.addEventListener("click", () => {
+    introButton.style.backgroundColor = "white";
+    introButton.style.color = "black";
     introP.style.display = "block";
     schoolP.style.display = "none";
     workP.style.display = "none";
+    schoolButton.style = "initial";
+    workButton.style = "initial";
 });
 
 schoolButton.addEventListener("click", () => {
+    schoolButton.style.backgroundColor = "white";
+    schoolButton.style.color = "black";
     introP.style.display = "none";
     schoolP.style.display = "block";
-    workP.style.display = "none";    
+    workP.style.display = "none";
+    introButton.style = "initial";
+    workButton.style = "initial";    
 });
 
 workButton.addEventListener("click", () => {
+    workButton.style.backgroundColor = "white";
+    workButton.style.color = "black";
     introP.style.display = "none";
     schoolP.style.display = "none";
     workP.style.display = "block";    
+    introButton.style = "initial";
+    schoolButton.style = "initial";
 })
+
